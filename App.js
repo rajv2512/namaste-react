8fb50eaf956044a90ad5463489d3e57636581f83
Raj verma <rajv3770@gmail.com>
2023-01-03 23:47:47 +0530

    Setup

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading1 = React.createElement( "h1",{ id: "title",},"Welcome to new Tech");
+
+const heading2 = React.createElement("h2",{id: "title",}, "Boom React");
+
+const container = React.createElement("div",{id: "container",}, [heading1,heading2]);
+
+console.log(heading1);
+
+const root = ReactDOM.createRoot(document.getElementById("react1"));
+
+root.render(container);
